9c56d6849894708b73d35e9207bd951f626732c3
JoaoDias <joaodiasworking@gmail.com>
2024-04-13 16:31:17 -0300

    feat: Building Modern Web Applications with Go - Trevor Sawler - s2m24

--- course-building-mordern-web-app-golang/web-apps/hello-world/main.go
+++ course-building-mordern-web-app-golang/web-apps/hello-world/main.go
@@ -1,20 +1,55 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
 
+const portNumber = ":8080"
 
+// Home is the home page handler
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the home page")
+}
 
+// About is the about page handler
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := addValues(2, 2)
+	_, _ = fmt.Fprintf(w, "This is the about page and 2 + 2 = %d", sum)
+}
+
+func Divide(w http.ResponseWriter, r *http.Request) {
+	f, err := divideValues(100.0, 10.0)
+	if err != nil {
+		fmt.Fprintf(w, "Error: %s", err.Error())
+		return
+	}
+
+	fmt.Fprintf(w, "%f divided by %f is %f", 100.0, 10.0, f)
+
+}
+
+func divideValues(x, y float32) (float32, error) {
+	var result float32
+	if y <= 0 {
+		return result, errors.New("cannot divide by zero or less than zero")
+	}
+	result = x / y
+	return result, nil
+}
+
+// addValues adds two integers and returns the result
+func addValues(x, y int) int {
+	return x + y
+}
+
+// main is the entry point for the application
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello, World!")
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
-	})
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+	http.HandleFunc("/divide", Divide)
 
-	_ = http.ListenAndServe(":8080", nil)
+	fmt.Printf("Starting application on port %s", portNumber)
+	_ = http.ListenAndServe(portNumber, nil)
 }
